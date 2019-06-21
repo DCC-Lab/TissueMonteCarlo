909bcf4a7399b40ad3c3c30ed9d16531b491d10d
Daniel Cote <dccote@novajo.ca>
2019-06-21 03:59:26 +0200

    MOst polmc vector operations

--- Python/vector.py
+++ Python/vector.py
@@ -57,3 +57,55 @@ class Vector:
 
     def dotProduct(self, vector):
         return np.dot(self.v, vector)
+
+    def normalizedCrossProduct(self, vector):
+        productNorm = self.norm() * vector.norm()
+        return np.cross(self.v, vector) / np.sqrt(productNorm)
+
+    def normalizedDotProduct(self, vector):
+        productNorm = self.norm() * vector.norm()
+        return np.dot(self.v, vector) / np.sqrt(productNorm)
+
+    def orientedAngleBetween(self, u, v, w):
+        sinPhi = u.normalizedCrossProduct(v)
+        sinPhiAbs = abs(sinPhi)
+        phi = np.arcsin(sinPhiAbs);
+    
+        if u.dotProduct(v) <= 0:
+            phi = PI-phi;
+
+        if sinPhi.dotProduct(w) <= 0:
+            phi *= -1
+    
+        return phi;
+
+    def rotateAroundX(self, inPhi):
+        v = Vector(self.v)
+        u = Vector()
+        
+        c = np.cos(inPhi);
+        s = np.sin(inPhi);
+    
+        self.y = c * v.y - s * v.z;
+        self.z = s * v.y + c * v.z;
+
+    def rotateAroundY(self, inPhi):
+        v = Vector(self.v)
+        u = Vector()
+        
+        c = np.cos(inPhi);
+        s = np.sin(inPhi);
+    
+        self.x = c * v.y + s * v.z;
+        self.z = -s * v.y + c * v.z;
+
+    def rotateAroundZ(self, inPhi):
+        v = Vector(self.v)
+        u = Vector()
+        
+        c = np.cos(inPhi);
+        s = np.sin(inPhi);
+    
+        self.x = c * v.x - s * v.y;
+        self.y = s * v.x + c * v.y;
+        self.z = v.z;
