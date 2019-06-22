0f01d8625ceb984486edc78e4706c3888c09fad6
Daniel Cote <dccote@novajo.ca>
2019-06-22 19:12:43 +0200

    Expansion of vector and matrix to use float3, float3x3 etc...

--- swift/MonteCarlo3D/MonteCarlo3D-old/BulkMaterial.swift
+++ swift/MonteCarlo3D/MonteCarlo3D-old/BulkMaterial.swift
@@ -9,28 +9,28 @@
 import Foundation
 import SceneKit
 
-let infiniteDistance:Float = 1e4
+let infiniteDistance:float = 1e4
 
-extension Float {
+extension float {
     func isInfinite() -> Bool {
         return self > infiniteDistance
     }
 }
 
 class BulkMaterial  {
-    fileprivate var randomTable = [Float](repeating: 0, count: 65536)
+    fileprivate var randomTable = [float](repeating: 0, count: 65536)
     fileprivate var randomIndex:Int = 0
 
-    var mu_s:Float
-    var mu_a:Float
-    var mu_t:Float
-    var index:Float
-    var albedo:Float
+    var mu_s:float
+    var mu_a:float
+    var mu_t:float
+    var index:float
+    var albedo:float
     var description: String {
         return " µs=\(mu_s) µa=\(mu_a) index=\(index)"
     }
     
-    init(mu_s:Float, mu_a:Float, index:Float) {
+    init(mu_s:float, mu_a:float, index:float) {
         self.mu_s = mu_s
         self.mu_a = mu_a
         self.mu_t = mu_a + mu_s
@@ -40,7 +40,7 @@ class BulkMaterial  {
             self.albedo = mu_a/mu_t
         }
         for i in 0...65535 {
-            randomTable[i] = Float.random(in:0...1)
+            randomTable[i] = float.random(in:0...1)
         }
         randomIndex = Int.random(in: 0...65535)
     }
@@ -50,7 +50,7 @@ class BulkMaterial  {
         photon.decreaseWeightBy(delta)
     }
     
-    func randomFloat() -> Float {
+    func randomfloat() -> float {
         randomIndex += 1
         if randomIndex == 65536 {
             randomIndex = Int.random(in: 0...65535)
@@ -58,18 +58,18 @@ class BulkMaterial  {
         return randomTable[randomIndex]
     }
     
-    func randomScatteringDistance() -> Float {
+    func randomScatteringDistance() -> float {
         if mu_t == 0 {
             return infiniteDistance
         }
         
-        let n = randomFloat()
+        let n = randomfloat()
         let d = -log(n) / mu_t
 
         return d
     }
 
-    func randomScatteringAngles() -> (Float, Float) {
+    func randomScatteringAngles() -> (float, float) {
         return (0,0)
     }
 }
--- swift/montecarlo/montecarlo/SceneKit-exensions/Vector.swift
+++ swift/montecarlo/montecarlo/SceneKit-exensions/Vector.swift
@@ -8,13 +8,14 @@
 
 import Foundation
 import SceneKit
+import simd
 
-typealias Float = CGFloat
+typealias float = CGFloat
 typealias Vector = SCNVector3
 typealias Vector3D = Vector
 typealias v⃗ = Vector
 
-let π = Float(3.1415926535)
+let π = float(3.1415926535)
 let xHat = Vector(x: 1, y: 0, z: 0)
 let yHat = Vector(x: 0, y: 1, z: 0)
 let zHat = Vector(x: 0, y: 0, z: 1)
@@ -29,7 +30,7 @@ enum Axis:Int {
 }
 
 extension Vector {
-    init(_ x:Float,_ y:Float,_ z:Float) {
+    init(_ x:float,_ y:float,_ z:float) {
         self.init(x:x, y:y, z:z)
     }
     
@@ -37,7 +38,7 @@ extension Vector {
         return "(\(x),\(y),\(z))"
     }
     
-    subscript(index: Axis) -> Float {
+    subscript(index: Axis) -> float {
         get {
             switch index {
             case .X:
@@ -61,15 +62,15 @@ extension Vector {
         }
     }
     
-    func norm() -> Float {
+    func norm() -> float {
         return x*x + y*y + z*z
     }
     
-    func abs() -> Float {
+    func abs() -> float {
         return sqrt(x*x + y*y + z*z)
     }
     
-    mutating func normalize() throws {
+    mutating func normalize() throws -> Vector{
         let value = sqrt(x*x + y*y + z*z)
 
         if value != 0 {
@@ -79,19 +80,20 @@ extension Vector {
         } else {
             throw VectorError.UnexpectedNil
         }
+        return self
     }
     
-    mutating func addScaledVector(_ theVector:Vector, scale theScale:Float) {
+    mutating func addScaledVector(_ theVector:Vector, scale theScale:float) {
         x += theVector.x * theScale;
         y += theVector.y * theScale;
         z += theVector.z * theScale;
     }
     
-    func dotProduct(_ theVector : Vector ) -> Float {
+    func dotProduct(_ theVector : Vector ) -> float {
         return x * theVector.x + y * theVector.y + z * theVector.z;
     }
     
-    func normalizedDotProduct(_ theVector: Vector ) -> Float {
+    func normalizedDotProduct(_ theVector: Vector ) -> float {
         var prod = self.dotProduct(theVector)
         
         let norm_u = norm()
@@ -132,13 +134,13 @@ extension Vector {
         return t;
     }
     
-    func tripleProduct(v : Vector, w : Vector) -> Float {
+    func tripleProduct(v : Vector, w : Vector) -> float {
         let cp = crossProduct(v)
         
         return cp.dotProduct(w)
     }
     
-    func orientedAngleWith(_ y:Vector , aroundAxis r:Vector ) -> Float {
+    func orientedAngleWith(_ y:Vector , aroundAxis r:Vector ) -> float {
         let sinPhi = self.normalizedCrossProduct(y)
         
         var phi = asin(sinPhi.abs())
@@ -154,7 +156,7 @@ extension Vector {
         return phi;
     }
     
-    func distanceToPlaneWithOrigin(origin v0: Vector, normal vn:Vector, alongVector vd:Vector )-> Float {
+    func distanceToPlaneWithOrigin(origin v0: Vector, normal vn:Vector, alongVector vd:Vector )-> float {
         return -(vn.x * (x - v0.x) + vn.y * (y - v0.y) + vn.z * (z - v0.z) ) / (vn.x * vd.x + vn.y * vd.y + vn.z * vd.z)
     }
     
@@ -178,7 +180,7 @@ extension Vector {
         return false
     }
     
-    mutating func rotateAroundX(_ inPhi: Float) {
+    mutating func rotateAroundX(_ inPhi: float) {
         let c = cos(inPhi)
         let s = sin(inPhi)
         let tempY = y
@@ -187,7 +189,7 @@ extension Vector {
         z = s * tempY + c * z
     }
     
-    mutating func rotateAroundY(_ inPhi: Float) {
+    mutating func rotateAroundY(_ inPhi: float) {
         let c = cos(inPhi)
         let s = sin(inPhi)
         let tempZ = z
@@ -196,7 +198,7 @@ extension Vector {
         x = s * tempZ + c * x
     }
     
-    mutating func rotateAroundZ(_ inPhi: Float) {
+    mutating func rotateAroundZ(_ inPhi: float) {
         let c = cos(inPhi)
         let s = sin(inPhi)
         let tempX = x
@@ -205,16 +207,16 @@ extension Vector {
         y = s * tempX + c * y
     }
     
-    mutating func  rotateAroundAxis(_ u:Vector, byAngle theta:Float) {
+    mutating func  rotateAroundAxis(_ u:Vector, byAngle theta:float) {
         //http://en.wikipedia.org/wiki/Rotation_matrix
         
         let cosTheta = cos(theta)
         let sinTheta = sin(theta)
         let oneMinusCosTheta = 1 - cosTheta
         
-        let ux:Float = u.x
-        let uy:Float = u.y
-        let uz:Float = u.z
+        let ux:float = u.x
+        let uy:float = u.y
+        let uz:float = u.z
         
         let X = x
         let Y = y
@@ -249,15 +251,15 @@ extension Vector {
         return Vector(x: left.x - right.x, y: left.y - right.y, z:left.z - right.z)
     }
     
-    static func * (left: Vector, scalar: Float) -> Vector {
+    static func * (left: Vector, scalar: float) -> Vector {
         return Vector(x: left.x * scalar, y: left.y * scalar, z:left.z * scalar)
     }
     
-    static func * (scalar: Float, left: Vector) -> Vector {
+    static func * (scalar: float, left: Vector) -> Vector {
         return Vector(x: left.x * scalar, y: left.y * scalar, z:left.z * scalar)
     }
     
-    static func / (left: Vector, scalar: Float) -> Vector {
+    static func / (left: Vector, scalar: float) -> Vector {
         return Vector(x: left.x / scalar, y: left.y / scalar, z:left.z / scalar)
     }
     
@@ -273,16 +275,243 @@ extension Vector {
         left.z -= right.z
     }
     
-    static func *= ( left: inout Vector, scalar: Float) {
+    static func *= ( left: inout Vector, scalar: float) {
         left.x *= scalar
         left.y *= scalar
         left.z *= scalar
     }
     
-    static func /= ( left: inout Vector, scalar: Float) {
+    static func /= ( left: inout Vector, scalar: float) {
         left.x /= scalar
         left.y /= scalar
         left.z /= scalar
     }
 
+}
+
+extension float3 {
+//    convenience init(_ x:Float,_ y:Float,_ z:Float) {
+//        self.init(float3(x,y,z))
+//    }
+
+    var x:Float {
+        get {
+            return self[0]
+        }
+    }
+
+    var y:Float {
+        get {
+            return self[1]
+        }
+    }
+
+    var z:Float {
+        get {
+            return self[2]
+        }
+    }
+
+//    public var description: String {
+//        return "(\(x),\(y),\(z))"
+//    }
+//
+    func norm() -> Float {
+        return simd.norm_one(self)
+    }
+
+    func abs() -> Float {
+        return simd.length(self)
+    }
+
+    func normalize() throws -> float3 {
+        return simd.normalize(self)
+    }
+
+    mutating func addScaledVector(_ theVector:float3, scale theScale:Float) {
+        self += theVector * theScale
+    }
+
+    func dotProduct(_ theVector : float3 ) -> Float {
+        return simd.dot(self,theVector)
+    }
+
+    func normalizedDotProduct(_ theVector : float3 ) -> Float {
+        var prod = simd.dot(self,theVector)
+
+        let norm_u = self.norm()
+        let norm_v = theVector.norm()
+
+        if norm_u != 0 && norm_v != 0 {
+            prod /= sqrt(norm_u * norm_v);
+        }
+
+        if prod > 1 {
+            return 1
+        } else if prod < -1 {
+            return -1
+        }
+
+        return simd.dot(self,theVector)
+    }
+
+    func crossProduct(_ theVector: float3) -> float3 {
+        return  simd.cross(self, theVector)
+    }
+
+    func normalizedCrossProduct(_ theVector: float3) -> float3 {
+        var prod = simd.cross(self, theVector)
+        
+        let norm_u = simd.norm_one(self)
+        let norm_v = simd.norm_one(theVector)
+
+        if norm_u != 0 && norm_v != 0 {
+            prod /= sqrt(norm_u * norm_v)
+        }
+
+        let norm_t = simd.norm_one(prod)
+        if norm_t > 1 {
+            prod /= sqrt(norm_t);
+        }
+
+        return float3(prod)
+    }
+
+//
+//    func tripleProduct(v : Vector, w : Vector) -> Float {
+//        let cp = crossProduct(v)
+//
+//        return cp.dotProduct(w)
+//    }
+//
+    func orientedAngleWith(_ y:float3 , aroundAxis r:float3 ) -> Float {
+        let sinPhi = self.normalizedCrossProduct(y)
+
+        var phi = asin(sinPhi.abs())
+
+        if self.dotProduct(y) <= 0 {
+            phi = .pi - phi
+        }
+
+        if sinPhi.dotProduct(r) <= 0 {
+            phi *= -1
+        }
+
+        return phi
+    }
+//
+//    func distanceToPlaneWithOrigin(origin v0: Vector, normal vn:Vector, alongVector vd:Vector )-> Float {
+//        return -(vn.x * (x - v0.x) + vn.y * (y - v0.y) + vn.z * (z - v0.z) ) / (vn.x * vd.x + vn.y * vd.y + vn.z * vd.z)
+//    }
+//
+    func isParallelTo(_ v:float3 ) -> Bool {
+        let dp = dotProduct(v)
+
+        if Swift.abs(dp/self.abs()/v.abs() - 1) <= 1e-5  {
+            return true
+        }
+
+        return false
+    }
+
+    func isPerpendicularTo(_ v:float3 ) -> Bool {
+        let dp = self.dotProduct(v)
+
+        if Swift.abs(dp)/self.abs()/v.abs() <= 1e-5 {
+            return true
+        }
+
+        return false
+    }
+
+    mutating func rotateAroundX(_ phi: Float) {
+        let c = cos(phi)
+        let s = sin(phi)
+        
+        self = float3(self[0], c * self[1] - s * self[2], s * self[1] + c * self[2])
+    }
+
+    mutating func rotateAroundY(_ phi: Float) {
+        let c = cos(phi)
+        let s = sin(phi)
+        
+        self = float3(s * self[2] + c * self[0], self[1], s * self[2] - s * self[0])
+    }
+
+    mutating func rotateAroundZ(_ phi: Float) {
+        let c = cos(phi)
+        let s = sin(phi)
+
+        self = float3(c * self[0] - s * self[1], s * self[0] + c * self[1], self[2])
+    }
+
+    mutating func rotateAroundAxis(_ u:float3, byAngle theta:Float) -> float3 {
+        let rot = float3x3.rotationMatrixAround(axis: u, angle: theta)
+        return rot * self
+    }
+    
+}
+
+struct Matrix {
+    var m:[float]
+    
+    init(array:[float]? = nil) {
+        if array != nil {
+            m = array!
+        } else {
+            m = [float](repeating: 0, count: 9)
+        }
+    }
+
+    static func matIndex(row:Int, col:Int) -> Int {
+        return col + 3*row
+    }
+    
+    static func * (left: Matrix, right: Matrix) -> Matrix {
+        // Symbolically: p(i,j) = Sum_u Sum_v left(u,j) * right(i,v)
+        var product = Matrix()
+        for j in 0...3 {
+            for i in 0...3 {
+                let p = matIndex(row:i,col:j)
+                for v in 0...3 {
+                    let r = matIndex(row:i,col:v)
+                    for u in 0...3 {
+                        let l = matIndex(row:u,col:j)
+                        product.m[p] += left.m[l] * right.m[r]
+                    }
+                }
+            }
+        }
+        return product
+    }
+
+    static func + (left: Matrix, right: Matrix) -> Matrix {
+        // Symbolically: p(i,j) = left(i,j) + right(i,j)
+        var sum = Matrix()
+        for j in 0...3 {
+            for i in 0...3 {
+                let s = matIndex(row:i,col:j)
+                sum.m[s] += left.m[s] * right.m[s]
+            }
+        }
+        return sum
+    }
+
+}
+
+extension float3x3 {
+    static func rotationMatrixAround(axis:float3, angle:Float) -> float3x3 {
+        let normalizedAxis = try! axis.normalize()
+        let ct = cosf(angle)
+        let st = sinf(angle)
+        let ci = 1 - ct
+        let x = normalizedAxis.x, y = normalizedAxis.y, z = normalizedAxis.z
+        
+        // https://github.com/MetalKit/metal/blob/master/raytracing/Transforms.swift
+        let rot = float3x3(
+            float3(    ct + x * x * ci,  y * x * ci + z * st,  z * x * ci - y * st),
+            float3(x * y * ci - z * st,      ct + y * y * ci,  z * y * ci + x * st),
+            float3(x * z * ci + y * st,  y * z * ci - x * st,      ct + z * z * ci))
+        return rot
+    }
 }
