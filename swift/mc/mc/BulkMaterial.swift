d83291e90794af76c2c3e730843305dd6a636895
Daniel Cote <dccote@novajo.ca>
2019-06-23 09:58:53 +0200

    Started metal MPS photon

--- swift/mc/mc/BulkMaterial.swift
+++ swift/mc/mc/BulkMaterial.swift
@@ -73,3 +73,56 @@ class BulkMaterial  {
         return (0,0)
     }
 }
+
+class BulkMaterialSIMD4  {
+    fileprivate var randomTable = [Float](repeating: 0, count: TableSize)
+    fileprivate var randomIndex:Int = 0
+    
+    var mu_s:Float
+    var mu_a:Float
+    var mu_t:Float
+    var index:Float
+    var albedo:Float
+    var description: String {
+        return " µs=\(mu_s) µa=\(mu_a) index=\(index)"
+    }
+    
+    init(mu_s:Float, mu_a:Float, index:Float) {
+        self.mu_s = mu_s
+        self.mu_a = mu_a
+        self.mu_t = mu_a + mu_s
+        self.index = index
+        self.albedo = 0
+        if mu_t != 0 {
+            self.albedo = mu_a/mu_t
+        }
+    }
+    
+    func absorbEnergy(_ photon:PhotonSIMD4) {
+        let delta = photon.weight * albedo
+        photon.decreaseWeightBy(delta)
+    }
+    
+    func randomFloat() -> Float {
+        randomIndex += 1
+        if randomIndex == TableSize {
+            randomIndex = Int.random(in: 0...TableSize)
+        }
+        return randomTable[randomIndex]
+    }
+    
+    func randomScatteringDistance() -> Float {
+        if mu_t == 0 {
+            return infiniteDistance
+        }
+        
+        let n = randomFloat()
+        let d = -log(n) / mu_t
+        
+        return d
+    }
+    
+    func randomScatteringAngles() -> (Float, Float) {
+        return (0,0)
+    }
+}
