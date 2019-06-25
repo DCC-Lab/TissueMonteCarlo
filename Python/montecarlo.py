4bf49e0fd32a3075aaf2f8c3a898a3d3f4202f29
Daniel Cote <dccote@novajo.ca>
2019-06-24 21:59:57 -0400

    Added a real-time update test

--- Python/montecarlo.py
+++ Python/montecarlo.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 import time
 
 if __name__ == "__main__":
-    N = 1000
+    N = 10000
     M = 21
-    mat = Material(mu_s=30, mu_a = 0.1, g = 0., L = (0.1, 0.1, 0.3), N = (M,M,M))
-
+    mat = Material(mu_s=30, mu_a = 1, g = 0., L = (0.1, 0.1, 0.3), N = (M,M,M))
+    plt.ion()
+    fig = plt.figure()
     startTime = time.time()
     photon = Photon()
     for i in range(N):
@@ -18,12 +19,14 @@ if __name__ == "__main__":
         while photon.isAlive:
             d = mat.getScatteringDistance(photon)
             (theta, phi) = mat.getScatteringAngles(photon)
-            photon.moveBy(d)
             photon.scatterBy(theta, phi)
+            photon.moveBy(d)
             mat.absorbEnergy(photon)
             photon.roulette()
-    
+        if i % 100 == 0:
+            plt.imshow(np.log(mat.energy[:,:,int((M-1)/2)]+0.0001),cmap='hsv')
+            plt.title("{0} photons".format(i))
+            plt.show()
+            plt.pause(0.0001)
     elapsed = time.time() - startTime
     print('{0:.1f} s for {2} photons, {1:.1f} ms per photon'.format(elapsed, elapsed/N*1000, N))
-    plt.imshow(np.log(mat.energy[:,int((M-1)/2),:]+0.0001))
-    plt.show()
