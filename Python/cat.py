58c4b2b25bce8735ffcec97137aaf32bf53c9b66
Daniel Côté <dccote@novajo.ca>
2020-10-14 23:41:52 -0400

    removed old files (category and fast lib)

--- Python/cat.py
+++
@@ -1,9 +0,0 @@
-from category import category
-
-class Daniel:
-    def more1():
-        print("Blabla")
-
-@category(Daniel)
-def all(self):
-    print("gapouet")
--- Python/category.py
+++
@@ -1,8 +0,0 @@
-class category(object):
-    def __init__(self, mainModule, override = True):
-        self.mainModule = mainModule
-        self.override = override
-
-    def __call__(self, function):
-        if self.override or function.__name__ not in dir(self.mainModule):
-            setattr(self.mainModule, function.__name__, function)
